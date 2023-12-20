01fae8b4c7ffe2fbeada006accc1121cc5a60938
YelizBozkurt <141838730+YelizBozkurt@users.noreply.github.com>
2023-12-20 20:55:12 +0300

    Tax Amount
    Creating "Tax Amaount" app

---
+++ Java101/Program_to_Calculate_Tax_Amount/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double amount = 3.75;
+        System.out.println(amount);
+
+        }
+    }
+}
