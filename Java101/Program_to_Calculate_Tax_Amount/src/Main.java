67d947097d010a18d33701f8bdda669ef00919f2
YelizBozkurt <141838730+YelizBozkurt@users.noreply.github.com>
2024-01-17 21:54:17 +0300

    Fix errors
    Fix errors

--- Java101/Program_to_Calculate_Tax_Amount/src/Main.java
+++ Java101/Program_to_Calculate_Tax_Amount/src/Main.java
@@ -1,9 +1,14 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        double amount = 3.75;
-        System.out.println(amount);
+    public static void main(String[]args){
+        double tutar , kdoranı = 0.18;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Lütfen tutar giriniz: " );
+        tutar=input.nextDouble();
+        double kdvoranı=tutar*kdoranı;
+        double  kdvliTutar=tutar+kdoranı;
+        System.out.println(kdvliTutar);
 
-        }
+    }
 }
